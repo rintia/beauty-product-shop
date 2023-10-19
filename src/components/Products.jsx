44df885d8e2492b935efef2d73b8e56298b8bc59
Rintia Islam <rintiaislam110@gmail.com>
2023-10-20 01:50:03 +0600

    No products available message shown

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -31,9 +31,13 @@ const Products = () => {
         <div className="min-h-screen" >
             <Slider brand={brand} ></Slider>
             <div className="max-w-screen-xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 px-4 lg:px-0 gap-4 my-24">
-                {
+                {   selected.length > 0 ?
                     selected.map(product => <ProductCard product={product}
                          key={product._id}></ProductCard>)
+                        :
+                        <div className="col-span-4 ">
+                            <h1 className="text-4xl text-center">No Products Available</h1>
+                        </div>
                 }
             </div>
         </div>
