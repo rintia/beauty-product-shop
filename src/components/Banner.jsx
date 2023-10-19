2517c6ae2adf5075ff3ce2f9bfca3f8c951042c8
Rintia Islam <rintiaislam110@gmail.com>
2023-10-19 15:06:01 +0600

    banner added

---
+++ src/components/Banner.jsx
@@ -0,0 +1,18 @@
+
+
+const Banner = () => {
+    return (
+        <div className="hero h-96 " style={{backgroundImage: 'url(https://i.ibb.co/g9Gyv4J/banner2.jpg)'}}>
+  <div className="hero-overlay "></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="max-w-md">
+      <h1 className="mb-5 text-7xl  font-bold">A Flawless Look</h1>
+      <p className="mb-5">Make up products all around the world are just a tap away </p>
+
+    </div>
+  </div>
+</div>
+    );
+};
+
+export default Banner;
